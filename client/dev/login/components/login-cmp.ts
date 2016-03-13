73974cac67eca0f477ec6a4c87d7993182e58e59
georgesuveti <george.suveti@irian.ro>
2016-03-13 13:03:34 +0200

    remove login service

--- client/dev/login/components/login-cmp.ts
+++ client/dev/login/components/login-cmp.ts
@@ -12,7 +12,7 @@ import {
     Control
 } from 'angular2/common';
 
-import {LoginService} from '../services/login-service';
+//import {LoginService} from '../services/login-service';
 type Todo = {
     todoMessage: string;
     _id: string;
@@ -21,49 +21,49 @@ type Todo = {
 @Component({
     selector: 'login-cmp',
     templateUrl: 'client/dev/login/templates/login.html',
-    providers: [LoginService],
+    //providers: [LoginService],
 })
 export class LoginCmp implements OnInit {
     title:string = "ng2do";
     todos:Todo[] = [];
     todoForm:ControlGroup;
 
-    constructor(@Inject(FormBuilder) fb:FormBuilder, @Inject(LoginService) private _loginService:LoginService) {
-        this.todoForm = fb.group({
-            "todoMessage": ["", Validators.required]
-        });
+    constructor(@Inject(FormBuilder) fb:FormBuilder) {
+        //this.todoForm = fb.group({
+        //    "todoMessage": ["", Validators.required]
+        //});
     }
 
     ngOnInit() {
-        this._getAll();
+        //this._getAll();
     }
-
-    private _getAll():void {
-        this._loginService
-            .getAll()
-            .subscribe((todos) => {
-                this.todos = todos;
-            });
-    }
-
-    add(message:string):void {
-        this._loginService
-            .add(message)
-            .subscribe((m) => {
-                this.todos.push(m);
-                (<Control>this.todoForm.controls['todoMessage']).updateValue("");
-            });
-    }
-
-    remove(id:string):void {
-        this._loginService
-            .remove(id)
-            .subscribe(() => {
-                this.todos.forEach((t, i) => {
-                    if (t._id === id)
-                        return this.todos.splice(i, 1);
-                });
-            })
-    }
+    //
+    //private _getAll():void {
+    //    this._loginService
+    //        .getAll()
+    //        .subscribe((todos) => {
+    //            this.todos = todos;
+    //        });
+    //}
+    //
+    //add(message:string):void {
+    //    this._loginService
+    //        .add(message)
+    //        .subscribe((m) => {
+    //            this.todos.push(m);
+    //            (<Control>this.todoForm.controls['todoMessage']).updateValue("");
+    //        });
+    //}
+    //
+    //remove(id:string):void {
+    //    this._loginService
+    //        .remove(id)
+    //        .subscribe(() => {
+    //            this.todos.forEach((t, i) => {
+    //                if (t._id === id)
+    //                    return this.todos.splice(i, 1);
+    //            });
+    //        })
+    //}
 
 }
