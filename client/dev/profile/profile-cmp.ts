a3b2bbb7bfd615eb530e1ded73cfbfee73189e50
Ionut <ionut.leustean@outlook.com>
2016-03-14 21:49:06 +0200

    company style

--- client/dev/profile/profile-cmp.ts
+++ client/dev/profile/profile-cmp.ts
@@ -1,17 +1,17 @@
 import {
-    Component,
-    View,
-    Inject,
-    OnInit,
-    Input
+  Component,
+  View,
+  Inject,
+  OnInit,
+  Input
 } from 'angular2/core';
 
 import {
-    Validators,
-    FormBuilder,
-    ControlGroup,
-    Control,
-    CORE_DIRECTIVES
+  Validators,
+  FormBuilder,
+  ControlGroup,
+  Control,
+  CORE_DIRECTIVES
 } from 'angular2/common';
 import {HTTP_PROVIDERS} from 'angular2/http';
 import { Cookie } from 'ng2-cookies/ng2-cookies';
@@ -19,63 +19,75 @@ import LoginService from '../login/login-service';
 import AboutService from '../about/components/about-service';
 
 @Component({
-    selector: 'profile-cmp',
-    templateUrl: 'client/dev/profile/profile.html',
+  selector: 'profile-cmp',
+  templateUrl: 'client/dev/profile/profile.html',
   styleUrls: ['client/dev/profile/styles/profile.css'],
   providers: [HTTP_PROVIDERS, LoginService, AboutService],
-    directives: [CORE_DIRECTIVES]
+  directives: [CORE_DIRECTIVES]
 })
 export class ProfileCmp implements OnInit {
-    companies = [{
-        name: null,
-        description: null,
-        web: null
-    }];
-    option = '';
-    token:string;
-    user = {};
-    model = {};
+  companies = [{
+    name: null,
+    description: null,
+    web: null
+  }];
+  option = '';
+  token:string;
+  user = {};
+  model = {};
 
-    constructor(private loginService:LoginService, private aboutService:AboutService) {
-        console.log("ProfileCmp");
-    }
+  checkCompanies = [];
 
-    ngOnInit() {
-        this.token = Cookie.getCookie('ppt');
-        this.loginService.getUser(this.token)
-            .subscribe(
-                data => {
-                    console.log('Authentication');
-                    console.log(data);
-                    if (!data.message) {
-                        this.user = data;
-                    }
-                },
-                err => console.log(err.json().message),
-                () => console.log('Authentication Complete')
-            );
+  constructor(private loginService:LoginService, private aboutService:AboutService) {
+    console.log("ProfileCmp");
+  }
 
-        this.aboutService.getCompaniesLight()
-            .subscribe(
-                data => {
-                    console.log(data);
-                    if (!data.message) {
-                        this.companies = data;
-                    }
-                },
-                err => console.log(err.json().message),
-                () => console.log('Authentication Complete')
-            );
-    }
+  ngOnInit() {
+    this.token = Cookie.getCookie('ppt');
+    this.loginService.getUser(this.token)
+      .subscribe(
+        data => {
+          console.log('Authentication');
+          console.log(data);
+          if (!data.message) {
+            this.user = data;
+          }
+        },
+        err => console.log(err.json().message),
+        () => console.log('Authentication Complete')
+      );
 
-    optionToggle(option) {
-        console.log(option);
-        this.option = option;
-    }
+    this.aboutService.getCompaniesLight()
+      .subscribe(
+        data => {
+          console.log(data);
+          if (!data.message) {
+            this.companies = data;
+
+            for(let i = 0; i < this.companies.length; i++ ){
+              this.checkCompanies[this.companies[i].name] = false;
+
+            }
+          }
+        },
+        err => console.log(err.json().message),
+        () => console.log('Authentication Complete')
+      );
+  }
+
+  optionToggle(option) {
+    console.log(option);
+    this.option = option;
+  }
 
 
-    saveProfile() {
-        console.log(this.model);
-    }
+  saveProfile() {
+    console.log(this.model);
+  }
+
+  toggleCompany(company){
+    this.checkCompanies[company] = !this.checkCompanies[company];
+    console.log(this.checkCompanies);
+  }
 
 }
