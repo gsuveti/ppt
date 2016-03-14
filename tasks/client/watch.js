49808d89d82c3ce5049d1f6c2b86bd4e8b491f08
Ionut <ionut.leustean@outlook.com>
2016-03-14 20:00:44 +0200

    "Solicit loc de practica propus de mine" - design

--- tasks/client/watch.js
+++ tasks/client/watch.js
@@ -15,11 +15,11 @@ gulp.task(tasks.CLIENT_WATCH, [tasks.CLIENT_LESS, tasks.CLIENT_BUILD_TS_DEV, tas
   browserSync({proxy: "http://localhost:3333", reloadDelay: 1000});
 
   let _watchable = [];
-
-  _watchable.push(TS);
-  _watchable.push(LESS);
-  _watchable.push(CSS);
-  _watchable.push(HTML);
+  //
+  //_watchable.push(TS);
+  //_watchable.push(LESS);
+  //_watchable.push(CSS);
+  //_watchable.push(HTML);
 
   return gulp.watch(_watchable, [tasks.CLIENT_LESS, tasks.CLIENT_BUILD_TS_DEV, tasks.CLIENT_BROWSER_SYNC]);
 });
